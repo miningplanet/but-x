b14ae8d5636fd75e8e464323d9ba50ef0914a8c2
miningplanet <mining@planet.ms>
2023-01-28 17:34:58 +0100

    Extend TX data model

--- models/tx.js
+++ models/tx.js
@@ -3,13 +3,18 @@ var mongoose = require('mongoose'),
 
 var TxSchema = new Schema({
   txid: { type: String, lowercase: true, unique: true, index: true},
+  version: {type: Number, default: 0 },
+  tx_type: { type: String, default: null },
+  size: {type: Number, default: 0 },
+  locktime: {type: Number, default: 0 },
+  instantlock: {type: Boolean, default: false },
+  chainlock: {type: Boolean, default: false },
   vin: { type: Array, default: [] },
   vout: { type: Array, default: [] },
   total: { type: Number, default: 0, index: true },
   timestamp: { type: Number, default: 0, index: true },
   blockhash: { type: String, index: true },
   blockindex: {type: Number, default: 0, index: true},
-  tx_type: { type: String, default: null },
   op_return: { type: String, default: null }
 }, {id: false});
 
