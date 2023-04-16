a144f1b5ea0d869c3f541ce99c02611350316c2c
miningplanet <mining@planet.ms>
2023-04-16 10:59:39 +0200

    Multicoin benchmark script.

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,134 +1,151 @@
+/**
+ * Benchmarks the system.
+ *
+ * !!!
+ * 
+ * Deletes all TX and addresses in he database, and restores it 
+ * for the given number of blocks by the deamon starting at height 0.
+ * 
+ * Usage:
+ * 
+ * node scripts/benchmark.js [net] [blocks]
+ * 
+ * [net] = chain, default 'testnet' or settings.dbs[1].
+ * [blocks] = number of blocks, default 5000.
+ */
 var mongoose = require('mongoose'),
-    db = require('../lib/database'),
-    Tx = require('../models/tx'),
-    Address = require('../models/address'),
     settings = require('../lib/settings'),
     lib = require('../lib/explorer'),
-    // Stats = require('../models/stats'),
-    async = require('async');
+    async = require('async'),
+    instance = require('../bin/instance'),
+    db = require('../lib/database');
 
-var COUNT = 5000; // number of blocks to index
+const { StatsDb, AddressDb, AddressTxDb, TxDb, RichlistDb } = require('../lib/database');
+
+var net = settings.dbs[1].id
+if (process.argv[2] != null && process.argv[2] != '') {
+  net = settings.getNetOrNull(process.argv[2])
+  console.log("Use chain %s", net)
+}
+if (net == null) {
+  console.error("Chain '%s' not found. Exit.", process.argv[2])
+  exit(999)
+}
+
+var numberOfBlocks = 5000;
+if (process.argv[3] != null && process.argv[3] != '') {
+  numberOfBlocks = parseInt(process.argv[3])
+  console.log("Use number of blocks %d", numberOfBlocks)
+}
 
 function exit(exitCode) {
   mongoose.disconnect();
   process.exit(exitCode);
 }
 
-// TODO: DB benchmark
-const dbs = settings.getDbs()
+TxDb[net].deleteMany({}, function(err) {
+  AddressDb[net].deleteMany({}, function(err2) {
+    var s_timer = new Date().getTime();
 
-mongoose.set('strictQuery', true);
+    // updates tx, address & richlist db's
+    function update_tx_db(coin, start, end, txes, timeout, check_only, cb) {
+      var complete = false;
+      var blocks_to_scan = [];
+      var task_limit_blocks = settings.sync.block_parallel_tasks;
+      var task_limit_txs = 1;
 
-mongoose.connect(dbs[0], function(err) {
-  if (err) {
-    console.log('Error: Unable to connect to database: %s', dbString);
-    exit(999);
-  }
+      // fix for invalid block height (skip genesis block as it should not have valid txs)
+      if (typeof start === 'undefined' || start < 1)
+        start = 1;
 
-  Tx.deleteMany({}, function(err) {
-    db.AddressDb[net].deleteMany({}, function(err2) {
-      var s_timer = new Date().getTime();
+      if (task_limit_blocks < 1)
+        task_limit_blocks = 1;
 
-      // updates tx, address & richlist db's
-      function update_tx_db(coin, start, end, txes, timeout, check_only, cb) {
-        var complete = false;
-        var blocks_to_scan = [];
-        var task_limit_blocks = settings.sync.block_parallel_tasks;
-        var task_limit_txs = 1;
+      for (i = start; i < (end + 1); i++)
+        blocks_to_scan.push(i);
 
-        // fix for invalid block height (skip genesis block as it should not have valid txs)
-        if (typeof start === 'undefined' || start < 1)
-          start = 1;
+      async.eachLimit(blocks_to_scan, task_limit_blocks, function(block_height, next_block) {
+        if (!check_only && block_height % settings.sync.save_stats_after_sync_blocks === 0) {
+          StatsDb[net].updateOne({coin: coin}, {
+            last: block_height - 1,
+            txes: txes
+          }, function() {});
+        } else if (check_only) {
+          console.log('Checking block ' + block_height + '...');
+        }
 
-        if (task_limit_blocks < 1)
-          task_limit_blocks = 1;
+        lib.get_blockhash(block_height, function(blockhash) {
+          if (blockhash) {
+            lib.get_block(blockhash, function(block) {
+              if (block) {
+                async.eachLimit(block.tx, task_limit_txs, function(txid, next_tx) {
+                  TxDb[net].findOne({txid: txid}, function(err, tx) {
+                    if (tx) {
+                      setTimeout( function() {
+                        tx = null;
+                        next_tx();
+                      }, timeout);
+                    } else {
+                      db.save_tx(net, txid, block_height, function(err, tx_has_vout) {
+                        if (err)
+                          console.log(err);
+                        else
+                          console.log('%s: %s', block_height, txid);
 
-        for (i = start; i < (end + 1); i++)
-          blocks_to_scan.push(i);
+                        if (tx_has_vout)
+                          txes++;
 
-        async.eachLimit(blocks_to_scan, task_limit_blocks, function(block_height, next_block) {
-          if (!check_only && block_height % settings.sync.save_stats_after_sync_blocks === 0) {
-            db.StatsDb[net].updateOne({coin: coin}, {
-              last: block_height - 1,
-              txes: txes
-            }, function() {});
-          } else if (check_only) {
-            console.log('Checking block ' + block_height + '...');
-          }
-
-          lib.get_blockhash(block_height, function(blockhash) {
-            if (blockhash) {
-              lib.get_block(blockhash, function(block) {
-                if (block) {
-                  async.eachLimit(block.tx, task_limit_txs, function(txid, next_tx) {
-                    Tx.findOne({txid: txid}, function(err, tx) {
-                      if (tx) {
                         setTimeout( function() {
                           tx = null;
                           next_tx();
                         }, timeout);
-                      } else {
-                        db.save_tx(net, txid, block_height, function(err, tx_has_vout) {
-                          if (err)
-                            console.log(err);
-                          else
-                            console.log('%s: %s', block_height, txid);
-
-                          if (tx_has_vout)
-                            txes++;
-
-                          setTimeout( function() {
-                            tx = null;
-                            next_tx();
-                          }, timeout);
-                        });
-                      }
-                    });
-                  }, function() {
-                    setTimeout( function() {
-                      blockhash = null;
-                      block = null;
-                      next_block();
-                    }, timeout);
+                      });
+                    }
                   });
-                } else {
-                  console.log('Block not found: %s', blockhash);
-
+                }, function() {
                   setTimeout( function() {
+                    blockhash = null;
+                    block = null;
                     next_block();
                   }, timeout);
-                }
-              }, net);
-            } else {
-              setTimeout( function() {
-                next_block();
-              }, timeout);
-            }
-          }, net);
+                });
+              } else {
+                console.log('Block not found: %s', blockhash);
+
+                setTimeout( function() {
+                  next_block();
+                }, timeout);
+              }
+            }, net);
+          } else {
+            setTimeout( function() {
+              next_block();
+            }, timeout);
+          }
+        }, net);
+      }, function() {
+        StatsDb[net].updateOne({coin: coin}, {
+          last: end,
+          txes: txes
         }, function() {
-          db.StatsDb[net].updateOne({coin: coin}, {
-            last: end,
-            txes: txes
-          }, function() {
-            return cb();
-          });
+          return cb();
         });
-      }
+      });
+    }
 
-      update_tx_db(settings.coin.name, 1, COUNT, 0, settings.sync.update_timeout, false, function() {
-        var e_timer = new Date().getTime();
+    update_tx_db(settings.coin.name, 1, numberOfBlocks, 0, settings.sync.update_timeout, false, function() {
+      var e_timer = new Date().getTime();
 
-        Tx.countDocuments({}, function(txerr, txcount) {
-          dbAddressDb[net].countDocuments({}, function(aerr, acount) {
-            var stats = {
-              tx_count: txcount,
-              address_count: acount,
-              seconds: (e_timer - s_timer)/1000,
-            };
+      TxDb[net].countDocuments({}, function(txerr, txcount) {
+        AddressDb[net].countDocuments({}, function(aerr, acount) {
+          var stats = {
+            tx_count: txcount,
+            address_count: acount,
+            seconds: (e_timer - s_timer)/1000,
+          };
 
-            console.log(stats);
-            exit(0);
-          });
+          console.log(stats);
+          exit(0);
         });
       });
     });
