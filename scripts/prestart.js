1504d755239a3a1c44456337bd4e5f4c5b2a6273
miningplanet <mining@planet.ms>
2023-11-19 12:53:59 +0100

    Log version at startup + small re-actorings in prestart script.

--- scripts/prestart.js
+++ scripts/prestart.js
@@ -1,84 +1,97 @@
-const debug = require('debug')('debug');
-const minNodeVersionMajor = '17';
-const minNodeVersionMinor = '19';
-const minNodeVersionRevision = '0';
+const debug = require('debug')('debug')
+const package_metadata = require('../package.json')
+const minNodeVersionMajor = '17'
+const minNodeVersionMinor = '19'
+const minNodeVersionRevision = '0'
 
 // get the nodejs version
-var nodeVersion = process.version;
-var nodeVersionMajor = '0';
-var nodeVersionMinor = '0';
-var nodeVersionRevision = '0';
+var nodeVersion = process.version
+var nodeVersionMajor = '0'
+var nodeVersionMinor = '0'
+var nodeVersionRevision = '0'
 
-// check if the nodejs version # is blank or a very long string as that would usually indicate a problem
+// check node.js version.
 if (nodeVersion != null && nodeVersion != '' && nodeVersion.length < 16) {
-  // Remove the 'v' from the beginning of the version string
   if (nodeVersion.indexOf('v') == 0)
-    nodeVersion = nodeVersion.slice(1);
+    nodeVersion = nodeVersion.slice(1)
+  
+  const tokens = nodeVersion.split('.')
+  
+  if (tokens.length > 0)
+    nodeVersionMajor = tokens[0]
 
-  // split node version string into major, minor and revision
-  var splitVersion = nodeVersion.split('.');
+  if (tokens.length > 1)
+    nodeVersionMinor = tokens[1]
 
-  nodeVersionMajor = splitVersion[0];
+  if (tokens.length > 2)
+    nodeVersionRevision = tokens[2]
 
-  if (splitVersion.length > 1)
-    nodeVersionMinor = splitVersion[1];
-
-  if (splitVersion.length > 2)
-    nodeVersionRevision = splitVersion[2];
-
-  debug("Got node version '%s': %d.%d.%d.", nodeVersion, nodeVersionMajor, nodeVersionMinor, nodeVersionRevision)
+  debug("Got node.js version '%s': %d.%d.%d.", nodeVersion, nodeVersionMajor, nodeVersionMinor, nodeVersionRevision)
+} else {
+  console.error('Failed to get node.js version. Install node.js min. version %s.%s.%s.\nExiting now.', minNodeVersionMajor, minNodeVersionMinor, minNodeVersionRevision)
+  exit(1)
 }
 
-// check if the installed nodejs is an older version than supported by the explorer
-if (!(nodeVersionMajor > minNodeVersionMajor || (nodeVersionMajor == minNodeVersionMajor && (nodeVersionMinor > minNodeVersionMinor || (nodeVersionMinor == minNodeVersionMinor && nodeVersionRevision >= minNodeVersionRevision))))) {
-  console.log(`Please install an updated version of nodejs.\n\nInstalled: ${nodeVersion}\nRequired:  ${minNodeVersionMajor}.${minNodeVersionMinor}.${minNodeVersionRevision}`);
-  process.exit(0);
+// check if nodejs is older than min. version
+if (!(nodeVersionMajor > minNodeVersionMajor || (nodeVersionMajor == minNodeVersionMajor
+    && (nodeVersionMinor > minNodeVersionMinor || (nodeVersionMinor == minNodeVersionMinor 
+    && nodeVersionRevision >= minNodeVersionRevision)))))
+{
+  console.log('Please install an updated version of nodejs.\n\nInstalled: %s\nRequired:  %s.%s.%s', nodeVersion, minNodeVersionMajor, minNodeVersionMinor, minNodeVersionRevision)
+  process.exit(0)
 }
 
+// verify CLI arguments
 function check_argument_passed(cb) {
-  const pidName = (process.argv[2] != null && process.argv[2] != '' && (process.argv[2] == 'pm2') ? process.argv[2] : 'node');
+  const arg2 = process.argv[2]
+  const pidName = (arg2 != null && arg2 != '' && (arg2 == 'pm2') ? arg2 : 'node')
 
   // check 1st argument
-  if (process.argv[2] != null) {
-    const { exec } = require('child_process');
+  if (arg2 != null) {
+    const { exec } = require('child_process')
 
     // determine which argument was passed
-    switch (process.argv[2]) {
+    switch (arg2) {
       case 'pm2':
         // windows pm2 has problem loading locally, but other os's should work fine
-        const isWinOS = process.platform == 'win32';
+        const isWinOS = process.platform == 'win32'
         
         // run a cmd to check if pm2 is installed
         exec(`npm list${(isWinOS ? ' -g' : '')} pm2`, (err, stdout, stderr) => {
           // split stdout string by new line
-          var splitResponse = (stdout == null ? '' : stdout).split('\n').filter(element => element);
+          var splitResponse = (stdout == null ? '' : stdout).split('\n').filter(element => element)
 
           // check if the cmd result contains an @ symbol
           if (splitResponse[splitResponse.length - 1].indexOf('@') == -1) {
-            console.log('Installing pm2 module.. Please wait..');
+            console.log('Installing pm2 module.. Please wait..')
 
             // install pm2
             exec(`npm install pm2@latest${(isWinOS ? ' -g' : '')}`, (err, stdout, stderr) => {
               // always return the pidName for now without checking results
-              return cb(pidName);
-            });
+              return cb(pidName)
+            })
           } else
-            return cb(pidName);
-        });
-        break;
+            return cb(pidName)
+        })
+        break
       default:
         // argument not passed or unknown argument
-        return cb(pidName);
+        return cb(pidName)
     }
   } else
-    return cb(pidName);
+    return cb(pidName)
 }
+
+console.log('\x1B[34m')
+console.log('Launch but-eiquidus %s multicoin explorer', package_metadata.version)
+console.log('...')
+console.log('\x1B[0m')
 
 // check if an argument was passed into this script ???
 check_argument_passed(function(pidName) {
-  const execSync = require('child_process').execSync;
-  execSync('node ./scripts/compile_css.js', {stdio : 'inherit'});
+  const execSync = require('child_process').execSync
+  execSync('node ./scripts/compile_css.js', {stdio : 'inherit'})
 
   // finished pre-loading
-  process.exit(0);
-});
+  process.exit(0)
+})
