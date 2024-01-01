872e68eabce20de1d0d7c834b17eb58fee7bb505
miningplanet <mining@planet.ms>
2024-01-01 10:26:44 +0100

    Sum buys and sells on marlet page.

--- models/marketorder.js
+++ models/marketorder.js
@@ -1,7 +1,7 @@
 module.exports = new require('mongoose').Schema({
   ex: { type: String, index: true },
-  market: { type: String, index: true },
-  trade: { type: String, index: false },
+  market: { type: String, index: false },
+  trade: { type: String, index: true },
   type: { type: Number, default: -1, index: true },
   price: { type: Number, default: -1, index: false },
   quantity: { type: Number, default: -1, index: false },
