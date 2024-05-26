f5c5725f90c19e2c2e189834e97e61ee81f86b19
miningplanet <mining@planet.ms>
2024-05-26 16:09:40 +0200

    Move more sync related functions to sync script fix null pointer.

--- scripts/sync_chain.js
+++ scripts/sync_chain.js
@@ -636,6 +636,13 @@ function save_tx(net, txid, blockheight, cb) {
   }, net)
 }
 
+function hex_to_ascii(hex) {
+  var str = '';
+  for (var i = 0; i < hex.length; i += 2)
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  return str;
+}
+
 function update_address(hash, blockheight, txid, amount, type, cb, net=settings.getDefaultNet()) {
   var to_sent = false
   var to_received = false
