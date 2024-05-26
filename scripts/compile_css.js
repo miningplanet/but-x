895c153ecc5c6b43dbe9a0bd29749b300802a42e
miningplanet <mining@planet.ms>
2024-05-26 12:11:07 +0200

    Re-factor CSS compilation script.

--- scripts/compile_css.js
+++ scripts/compile_css.js
@@ -1,26 +1,54 @@
-const fs = require('fs');
-const settings = require('../lib/settings');
+/*
+* Generate minified CSS (.min.css).
+* 
+* Execute this script:
+*
+*   node ./scripts/compile_css.js
+*
+*     Generates
+*       - ./public/css/_theme-selector.scss'
+*       - ./public/css/style.min.css
+*
+*   node ./scripts/compile_css.js ${net}
+*
+*     Generates
+*       - ./public/css/${net}.min.css
+*/
 
-console.log('Compiling CSS.. Please wait..');
+const fs = require('fs')
+const settings = require('../lib/settings')
+const sass = require('sass')
+const net = process.argv[2]
 
-// ensure the selected theme is properly installed
-// TODO: Fix chain.
-const shared_pages = settings.get('mainnet', 'shared_pages')
-fs.writeFile('./public/css/_theme-selector.scss', `$theme-name: "${shared_pages.theme}";`, function (err) {
-  const sass = require('sass');
-
-  // generate minified css from style.scss file
-  const minified = sass.compile('./public/css/style.scss', {style: 'compressed'});
-
-  // save the minified css to file
-  fs.writeFile('./public/css/style.min.css', minified.css, function (err) {
-    // generate minified css from custom.scss file
-    const custom_minified = sass.compile('./public/css/custom.scss', {style: 'compressed'});
-
-    // save the minified css to file
-    fs.writeFile('./public/css/custom.min.css', custom_minified.css, function (err) {
-      // finished compiling css
-      process.exit(0);
-    });
-  });
-});
+if (process.argv.length < 3) {
+  console.log('Compiling base CSS file. To compile CSS for a chain. Use %s.', settings.getAllNet())
+  const shared_pages = settings.get(net, 'shared_pages')
+  var file = './public/css/_theme-selector.scss'
+  fs.writeFile(file, `$theme-name: "${shared_pages.theme}";`, function (err) {
+    console.log("Stored CSS theme selector to '%s'.", file)
+    const minified = sass.compile('./public/css/style.scss', { style: 'compressed' })
+    file = './public/css/style.min.css'
+    fs.writeFile(file, minified.css, function (err) {
+      console.log("Stored minified base CSS to '%s'.", file)
+      process.exit(0)
+    })
+  })
+} else {
+  if (settings.getNetOrNull(net)) {
+    console.log('Compiling CSS for net %s. Please wait..', net)  
+    const custom_minified = sass.compile('./public/css/' + net + '.scss', { style: 'compressed' })
+    const file = './public/css/' + net + '.min.css'
+    fs.writeFile(file, custom_minified.css, function (err) {
+      if (err) {
+        console.log("Failed to store minified CSS for net '%s' to '%s'.", net, file)
+        process.exit(1)
+      } else {
+        console.log("Stored minified CSS for net '%s' to '%s'.", net, file)
+        process.exit(0)
+      }
+    })
+  } else {
+    console.log("Net '%s' not found.", net)
+    process.exit(1)
+  }
+}
